5e3d5382151da7fc48cbca72ea517a01d699b882
Mat Brady <matbr8dy@gmail.com>
2017-08-18 14:17:00 -0400

    adding labels to columns and replacing quotes

--- index.js
+++ index.js
@@ -5,20 +5,62 @@ const argv = require("minimist")(process.argv.slice(2));
 const fileName = argv.filename || "issues.csv";
 
 const fields = [
-  "node.title",
-  "node.number",
-  "node.state",
-  "node.author.login",
-  "node.bodyText",
-  "node.url",
-  "node.milestone.id",
-  "node.labels.edges.node.name",
-  "node.comments.edges.node.bodyText",
-  "node.comments.edges.node.createdAt",
-  "node.comments.edges.node.author.avatarUrl",
-  "node.comments.edges.node.author.login",
-  "node.comments.edges.node.author.resourcepath",
-  "node.comments.edges.node.author.url"
+  {
+    value: "node.title",
+    label: "title"
+  },
+  {
+    value: "node.number",
+    label: "number"
+  },
+  {
+    value: "node.state",
+    label: "state"
+  },
+  {
+    value: "node.author.login",
+    label: "author"
+  },
+  {
+    value: "node.bodyText",
+    label: "body"
+  },
+  {
+    value: "node.url",
+    label: "url"
+  },
+  {
+    value: "node.milestone.id",
+    label: "milestone"
+  },
+  {
+    value: "node.labels.edges.node.name",
+    label: "label"
+  },
+  {
+    value: "node.comments.edges.node.bodyText",
+    label: "comment_body"
+  },
+  {
+    value: "node.comments.edges.node.createdAt",
+    label: "comment_createdAt"
+  },
+  // {
+  //   value: "node.comments.edges.node.author.avatarUrl",
+  //   label: ""
+  // },
+  {
+    value: "node.comments.edges.node.author.login",
+    label: "comment_author"
+  },
+  {
+    value: "node.comments.edges.node.author.resourcepath",
+    label: "comment_author_resource_path"
+  },
+  {
+    value: "node.comments.edges.node.author.url",
+    label: "comment_author_url"
+  }
 ];
 
 const client = new graphql.GraphQLClient("https://api.github.com/graphql", {
@@ -70,6 +112,13 @@ const query = `{
   }
 }`;
 
+function writeCsvFile(csv) {
+  fs.writeFile(fileName, csv, function(err) {
+    if (err) throw err;
+    console.log(`${fileName} file saved`);
+  });
+}
+
 client
   .request(query)
   .then(data => {
@@ -79,11 +128,9 @@ client
       includeEmptyRows: true,
       unwindPath: ["node.labels.edges", "node.comments.edges"]
     };
-    const csv = json2csv(json2csvConfig);
 
-    fs.writeFile(fileName, csv, function(err) {
-      if (err) throw err;
-      console.log(`${fileName} file saved`);
+    json2csv(json2csvConfig, (err, csvString) => {
+      writeCsvFile(csvString.replace(/[‘’]/g, `'`).replace(/[“”]/g, '"'));
     });
   })
   .catch(error => console.error(error));
